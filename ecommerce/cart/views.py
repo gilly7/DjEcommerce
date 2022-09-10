08f104dde26092384baafb9b959b389057430774
Gilbert Ng'eno <ngenogilbert07@gmail.com>
2022-09-10 18:47:07 +0300

    create remove from cart view

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -58,4 +58,39 @@ def add_to_cart(request, slug):
             user=request.user)
         order.orderitems.add(order_item)
         messages.info(request, "This item was added to your cart.")
-        return redirect("mainapp:home")
+        return redirect("mainapp:home")# Remove item from cart
+
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Product, slug=slug)
+    cart_qs = Cart.objects.filter(user=request.user, item=item)
+    if cart_qs.exists():
+        cart = cart_qs[0]
+        # Checking the cart quantity
+        if cart.quantity > 1:
+            cart.quantity -= 1
+            cart.save()
+        else:
+            cart_qs.delete()
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.orderitems.filter(item__slug=item.slug).exists():
+            order_item = Cart.objects.filter(
+                item=item,
+                user=request.user,
+            )[0]
+            order.orderitems.remove(order_item)
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("mainapp:home")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("mainapp:home")
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("core:home")
+    
+    
